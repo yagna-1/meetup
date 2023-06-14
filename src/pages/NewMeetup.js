2b26333a505bee0ab3e96b094b77fb9a69e88b48
yagna-1 <2020uec1319@mnit.ac.in>
2023-06-14 13:05:29 +0530

    initial

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch("https://meet-ups-1e33f-default-rtdb.firebaseio.com//meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      navigate("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
